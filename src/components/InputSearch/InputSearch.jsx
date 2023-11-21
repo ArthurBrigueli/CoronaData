0a9c35f587190cbeaa464867176aff66a38538d5
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-11-21 14:37:06 -0300

    feat: Movido o css do input para o module.css

--- src/components/InputSearch/InputSearch.jsx
+++ src/components/InputSearch/InputSearch.jsx
@@ -1,8 +1,12 @@
+import styles from './InputSearch.module.css'
+
+
+
 const InputSearch = ({onChange})=>{
 
     return(
         <>
-            <input type="search" name="" id="" placeholder="Pesquise por UF" onChange={(e)=>onChange(e.target.value)}/>
+            <input className={styles.InputSearch} type="search" name="" id="" placeholder="Pesquise por UF" onChange={(e)=>onChange(e.target.value)}/>
         </>
     )
 }
